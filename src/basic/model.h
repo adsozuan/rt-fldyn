f2ebdb82c07f814bbd3621abb426f7bc4ffde2ff
Adnan SOZUAN <adnan.sozuan@gmail.com>
2021-06-18 21:01:44 +0200

    Add initialization of vectors of model to zeros.

--- src/basic/model.h
+++ src/basic/model.h
@@ -6,12 +6,24 @@
 
 #include "basic.h"
 
+// TODO must fill all vectors with 0.0
+
 struct Model {
   using VectorkSize =
       xt::xtensor_fixed<double, xt::xshape<kGridSize + 2, kGridSize + 2>>;
 
+
   Model(double dt, double diffusion_rate, double viscosity)
-      : dt(dt), diffusion_rate(diffusion_rate), viscosity(viscosity) {}
+      : dt(dt), diffusion_rate(diffusion_rate), viscosity(viscosity) {
+    u.fill(0.0);
+    v.fill(0.0);
+    u_previous.fill(0.0);
+    v_previous.fill(0.0);
+    density.fill(0.0);
+    density_previous.fill(0.0);
+
+ }
+
 
   double dt;
   double diffusion_rate;
